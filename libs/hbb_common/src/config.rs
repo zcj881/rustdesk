e9d79cabdaecb27d6228bd9da205c3ba8bcb529d
zcj881 <34678672+zcj881@users.noreply.github.com>
2024-08-26 21:10:41 +0800

    config.rs

--- libs/hbb_common/src/config.rs
+++ libs/hbb_common/src/config.rs
@@ -100,8 +100,8 @@ const CHARS: &[char] = &[
     'm', 'n', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
 ];
 
-pub const RENDEZVOUS_SERVERS: &[&str] = &["rs-ny.rustdesk.com"];
-pub const PUBLIC_RS_PUB_KEY: &str = "OeVuKk5nlHiXp+APNn0Y3pC1Iwpwn44JGqrQCsWqmBw=";
+pub const RENDEZVOUS_SERVERS: &[&str] = &["chaojie.myds.me:21116"];
+pub const PUBLIC_RS_PUB_KEY: &str = "KEAeCvJ6KstFy5tE6uHv0YPwFV9XAJskTEBLAQ9NVag=";
 
 pub const RS_PUB_KEY: &str = match option_env!("RS_PUB_KEY") {
     Some(key) if !key.is_empty() => key,
