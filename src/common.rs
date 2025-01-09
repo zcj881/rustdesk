91c248af33e5ef3dd6927c6a16a8abd0e3632518
zcj881 <34678672+zcj881@users.noreply.github.com>
2025-01-09 17:18:08 +0800

    common.rs

--- src/common.rs
+++ src/common.rs
@@ -894,7 +894,7 @@ pub fn get_api_server(api: String, custom: String) -> String {
             return format!("http://{}", s);
         }
     }
-    "http://chaojie.myds.me:21114".to_owned()
+    "http://190305.xyz:21114".to_owned()
 }
 
 pub fn get_audit_server(api: String, custom: String, typ: String) -> String {
